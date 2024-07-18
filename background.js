b3bdfba0a7c7be8491ba74218ad08207369b2035
qinzhaoxuan <cherishxn@foxmail.com>
2024-07-18 15:51:26 +0800

    fix: add createNotification

--- background.js
+++ background.js
@@ -1,52 +1,50 @@
 chrome.runtime.onInstalled.addListener(async ({ reason }) => {
-  console.log('onInstalled', reason);
-  const tab = await getCurrentTab();
-  console.log(tab, '当前tab');
-});
+  console.log('onInstalled', reason)
+  const tab = await getCurrentTab()
+  console.log(tab, '当前tab')
+})
 
 chrome.action.onClicked.addListener((tab) => {
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
     files: ['connect.js', 'request.js', 'content.js'],
-    type: 'module',
-  });
-});
+    type: 'module'
+  })
+})
 
 chrome.runtime.onMessage.addListener((message, callback) => {
-  console.log(message, 'background.js ---- onMessage');
+  console.log(message, 'background.js ---- onMessage')
   if (message.action === 'fromContent') {
     messageCreate(message.message)
   }
-});
+})
 
-// 获取当前tab 
+// 获取当前tab
 async function getCurrentTab() {
-  let queryOptions = { active: true, lastFocusedWindow: true };
+  let queryOptions = { active: true, lastFocusedWindow: true }
   // `tab` will either be a `tabs.Tab` instance or `undefined`.
-  let [tab] = await chrome.tabs.query(queryOptions);
-  return tab;
+  let [tab] = await chrome.tabs.query(queryOptions)
+  return tab
 }
 
 // 向content-script主动发送消息
 function sendMessageToContentScript(message, callback) {
   getCurrentTabId((tabId) => {
     chrome.tabs.sendMessage(tabId, message, function (response) {
-      if (callback) callback(response);
-    });
-  });
+      if (callback) callback(response)
+    })
+  })
 }
-
 
 // 创建通知
 function messageCreate(message) {
   //创建一个通知面板
-  chrome.notifications.create(
-    Math.random() + '',
-    {
-      type: 'basic',
-      iconUrl: 'images/icon_32.png',
-      title: '自动化插件',
-      message: message
-    }
-  );
+  chrome.notifications.create(Math.random() + '', {
+    type: 'basic',
+    iconUrl: 'images/icon_32.png',
+    title: '自动化插件',
+    message: message
+  })
 }
+
+
--- content.js
+++ content.js
@@ -1208,4 +1208,31 @@ async function $handleError(error) {
   })
 }
 
+function createNotification(message) {
+  // 创建一个容器
+  const container = document.createElement('div')
+  container.style.position = 'fixed'
+  container.style.width = '100%'
+  container.style.top = '0'
+  container.style.left = '0'
+  container.style.backgroundColor = 'rgba(0, 0, 0, 0.7)'
+  container.style.color = 'white'
+  container.style.padding = '10px'
+  container.style.borderRadius = '5px'
+  container.style.zIndex = '10000'
+  container.style.fontFamily = 'Arial, sans-serif'
+  container.style.transition = '.3s'
+
+  // 设置消息内容
+  container.innerText = message
+
+  // 添加到文档
+  document.body.appendChild(container)
+
+  // 自动移除提示框
+  setTimeout(() => {
+    container.remove()
+  }, 5000) // 3秒后自动移除
+}
+
 // 工具函数 - 结束
